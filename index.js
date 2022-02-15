305ed0569f06d7ffdaba2e2acdbb711e528ddafa
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-02-15 19:48:20 +0600

    click and display in pin matcher is done. Addition is pending

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// To generate 6 degits pin 
+// To generate 6 digits pin 
 function getPin() {
     const pin = Math.round(Math.random() * 1000000);
     const pinToString = pin + ''; /* as number has no length but string has so if we want to get the length of a number we must convert that number into string */
@@ -16,3 +16,10 @@ function generatePin() {
     const getInputPin = document.getElementById('input-pin');
     getInputPin.value = generatedPin;
 }
+// Event deligation bubbling process to get input digits from calculator 
+document.getElementById('digits').addEventListener('click', function (event) {
+    const getClickedDigit = event.target;
+    const getDisplayInput = document.getElementById('display-digit');
+    getDisplayInput.value = getClickedDigit.innerText;
+
+})
